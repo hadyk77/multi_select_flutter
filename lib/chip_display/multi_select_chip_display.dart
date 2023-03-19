5cf1248bf1095f2a548fb3e1e89d09ad07a82480
hady mohamed <hadykamel3@gmail.com>
2023-03-19 16:38:22 +0300

    last commit

--- lib/chip_display/multi_select_chip_display.dart
+++ lib/chip_display/multi_select_chip_display.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import '../util/horizontal_scrollbar.dart';
 import '../util/multi_select_item.dart';
@@ -127,6 +129,7 @@ class MultiSelectChipDisplay<V> extends StatelessWidget {
   }
 
   Widget _buildItem(MultiSelectItem<V> item, BuildContext context) {
+    log("key:from package ${key}");
     return Container(
       padding: const EdgeInsets.all(2.0),
       child: ChoiceChip(
